8d5a77ee406d6a3ad4947859cbeabe259c646704
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2023-03-19 16:58:56 +0100

    Update id & final fixes

--- go.mod
+++
@@ -1,5 +0,0 @@
-module test3
-
-go 1.20
-
-require github.com/google/uuid v1.3.0
--- main.go
+++ main.go
@@ -1,88 +1,61 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/json" 
 	"fmt"
 	"io"
-	// "io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"github.com/google/uuid"
 )
 
-func getCatImageURL() string {
-	// ? Return catImage URL
-	fmt.Println("Getting cat image URL..." + getCatImage().URL)
-	return getCatImage().URL
+func main() {
+	var cat = getCat()
+	// ? Create path
+	var path string = "catImages/cat-" + cat.ID + ".jpg"
+
+	// ? Create file by path
+	file, err := os.Create(path)
+	errorCheck(err)
+    defer file.Close()
+
+	// ? Copy image to created file
+    _, err = io.Copy(file, getCatImage(cat))
+	errorCheck(err)
+
+	// ? Program end
+	fmt.Println("Image saved")
 }
 
-type CatImage struct {
+// ? Error handling
+func errorCheck(err error) {
+	if err != nil {log.Fatal("Fatal error -> " + err.Error())}
+}
+
+// * Cat staff
+type Cat struct {
 	ID string `json:"id"`
 	URL string `json:"url"`
 	Width int `json:"width"`
 	Height int `json:"height"`
 }
 
-func getCatImage() CatImage {
-	var catImages []CatImage
+func getCat() Cat {
+	var catArray []Cat
 
-	// ? https request, error handling
+	// ? https request
 	resp, err := http.Get("https://api.thecatapi.com/v1/images/search")
-	if err != nil {log.Fatal("https request error")}
+	errorCheck(err)
 	defer resp.Body.Close()
 
-	//fmt.Println(resp)
-
 	// ? Decode response to catIage object, error handling
-	err = json.NewDecoder(resp.Body).Decode(&catImages)
-	if err != nil {log.Fatal(err)}
-	return catImages[0]
+	err = json.NewDecoder(resp.Body).Decode(&catArray)
+	errorCheck(err)
+	return catArray[0]
 }
 
-func generateId() string {
-	// ? Read directory
-	// var dir string = "./catImages"
-	// files, err := ioutil.ReadDir(dir)
-	// if err != nil {log.Fatal(err)}
-
-	// // ? Count files in directory
-	// var count int = 0
-	// for _, file := range files {
-	// 	if !file.Mode().IsRegular() {count++}
-	// }
-
-	
-    var id uuid.UUID = uuid.New()
-	return id.String()
-}
-
-func ErrorCheck(err error) {
-	if err != nil {log.Fatal(err)}
-}
-
-func main() {
-	//var countOfCatImages int = getCatImagesCount()
-	//fmt.Println(countOfCatImages)
-
-	// ? Create path
-	var path string = "catImages/cat" + generateId() + ".jpg"
-
-	resp, err := http.Get(getCatImageURL())
-	ErrorCheck(err)
-	//err = os.MkdirAll(filepath.Dir(path), os.ModePerm)
-	file, err := os.Create(path)
-	ErrorCheck(err)
-    defer file.Close()
-
-    _, err = io.Copy(file, resp.Body)
-	ErrorCheck(err)
-
-    // var catFact CatFact
-	// // ? Decode data + error handling
-	// err = json.NewDecoder(resp.Body).Decode(&catFact)
-	// if err != nil {log.Fatal(err)} 
-
-    // fmt.Println(catFact.Fact)
-	fmt.Println("Image saved")
+func getCatImage(cat Cat) io.ReadCloser {
+	resp, err := http.Get(cat.URL)
+	errorCheck(err)
+	return resp.Body
 }
